338614294afd34738d9a0b80bdb3ed6b639961b7
Leszek Smentek <leszek.smentek@galdomedia.pl>
2011-01-03 12:17:27 +0100

    format fix

--- app/controllers/netzke_controller.rb
+++ app/controllers/netzke_controller.rb
@@ -60,15 +60,32 @@ class NetzkeController < ApplicationController
   # as well as the method of this component to be called, according to the double underscore notation.
   # E.g.: some_grid__post_grid_data.
   def method_missing(method_name)
-    component_name, *action = method_name.to_s.split('__')
-    component_name = component_name.to_sym
-    action = !action.empty? && action.join("__").to_sym
-
+    is_xls = false
+    respond_to do |format|
+       format.html {
+          component_name, *action = method_name.to_s.split('__')
+          component_name = component_name.to_sym
+          action = !action.empty? && action.join("__").to_sym
+       }
+       format.xls {
+          is_xls = true
+          component_name, *action = method_name.to_s.split('__')
+          component_name = component_name.to_sym
+          action = !action.empty? && action.join("__").to_sym
+       }
+    end
     if action
       w_instance = Netzke::Base.instance_by_config(Netzke::Core.session[:netzke_components][component_name])
       # only component's actions starting with "endpoint_" are accessible from outside (security)
       endpoint_action = action.to_s.index('__') ? action : "_#{action}_ep_wrapper"
-      render :text => w_instance.send(endpoint_action, params), :layout => false
+      unless is_xls
+      	render :text => w_instance.send(endpoint_action, params), :layout => false
+      else
+        headers['Content-Type'] = "application/vnd.ms-excel"
+        headers['Content-Disposition'] = 'attachment; filename="report.xls"'
+        headers['Cache-Control'] = ''
+        w_instance.send(endpoint_action, params)
+      end
     else
       super
     end
