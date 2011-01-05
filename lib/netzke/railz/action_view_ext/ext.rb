c20fa5d44c82fc926f7d96db943ef01589193451
Leszek Smentek <leszek.smentek@galdomedia.pl>
2011-01-05 12:38:05 +0100

    onReady delay

--- lib/netzke/railz/action_view_ext/ext.rb
+++ lib/netzke/railz/action_view_ext/ext.rb
@@ -42,7 +42,7 @@ module Netzke
 
             res << "Ext.onReady(function(){"
             res << content_for(:netzke_on_ready)
-            res << "});"
+            res << ", {delay: 100, single: true}});"
 
             javascript_tag res.join("\n")
           end
