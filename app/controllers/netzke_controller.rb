6b1d144f21f306a8d9d6f1ed2b47ec2a3a4b1464
Leszek Smentek <leszek.smentek@galdomedia.pl>
2011-01-06 16:33:08 +0100

    pdf support

--- app/controllers/netzke_controller.rb
+++ app/controllers/netzke_controller.rb
@@ -80,7 +80,7 @@ class NetzkeController < ApplicationController
              endpoint_action = action.to_s.index('__') ? action : "_#{action}_pdf_ep_wrapper"
              @data = w_instance.send(endpoint_action, params)
              #send_data render(:layout => false), :type=>"application/pdf", :disposition=>'attachment', :filename => 'raport.pdf'
-             render :layout => false 
+             render 'to_pdf', :layout => false 
           }
        end
     else
